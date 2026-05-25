ffcac52a9a609992012554a460fdf964836205b5
unknown <swetha@qnampelly.com>
2026-05-25 17:21:52 +0530

    latest files

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+image = "ami-091138d0f0d41ff90"
+type = "t3.micro"
---
+++ var.tf
@@ -0,0 +1,8 @@
+variable "type" {
+  description = "value of the instance type"
+}
+
+variable "image" {
+    description = "value of the ami"
+  
+}
