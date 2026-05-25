d76b89669386fe9875d7ad193b666e31eec8689f
unknown <swetha@qnampelly.com>
2026-05-25 17:31:58 +0530

    instance change

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
     }
 
-    resource "aws_instance" "example" {
+    resource "aws_instance" "test" {
         ami = var.image
         instance_type = var.type
       
